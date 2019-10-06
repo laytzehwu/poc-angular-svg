53642b1a4143501dcfebf5da074264232de56ced
Lay Tzeh Wu <laytzehwu@yahoo.com>
2019-10-06 11:42:33 +0800

    Fix first e2e spec

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -2,22 +2,28 @@ import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+    let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
-
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ahlaydiagram!');
-  });
-
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+    beforeEach(() => {
+        page = new AppPage();
+    });
+    
+    it('should display about by default', () => {
+        page.navigateTo();
+        expect(page.getTitleText()).toEqual('Prototype for SVG');
+        expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + 'about');
+        page.getNavLinks().then(items => {
+            expect(items.length).toBe(2);
+            expect(items[0].getText()).toBe('About');
+            expect(items[1].getText()).toBe('Diagram');
+        });
+    });
+    
+    afterEach(async () => {
+        // Assert that there are no errors emitted from the browser
+        const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+        expect(logs).not.toContain(jasmine.objectContaining({
+          level: logging.Level.SEVERE,
+        } as logging.Entry));
+    });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,15 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementArrayFinder } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
-  }
+    navigateTo() {
+        return browser.get(browser.baseUrl) as Promise<any>;
+    }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
-  }
+    getTitleText() {
+        return element(by.css('app-root h1')).getText() as Promise<string>;
+    }
+
+    getNavLinks(): ElementArrayFinder {
+        return element.all(by.css('app-root header nav a'));
+    }
 }
